f40bfa7ae0c562d5047333cb189e3e1522e3f0f9
Collins Kipkemoi <birkong1080@gmail.com>
2026-03-12 19:00:23 +0100

    clean the routes

--- server/src/routes/games.routes.ts
+++ server/src/routes/games.routes.ts
@@ -3,10 +3,7 @@ import { listGames, getGame } from "../controllers/games.controller.js";
 
 const router = Router();
 
-// GET /api/games
 router.get("/", listGames);
-
-// GET /api/games/:id
 router.get("/:id", getGame);
 
 export default router;
--- server/src/routes/stats.routes.ts
+++ server/src/routes/stats.routes.ts
@@ -3,10 +3,7 @@ import { getGameTypeStats, getHeatmap } from "../controllers/stats.controller.js
 
 const router = Router();
 
-// GET /api/stats/game-types
 router.get("/game-types", getGameTypeStats);
-
-// GET /api/stats/heatmap?gameType=x01
 router.get("/heatmap", getHeatmap);
 
 export default router;
